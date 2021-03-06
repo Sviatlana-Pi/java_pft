6b418217eb62e85b9091c564b283277d9a1dac3d
Sviatlana Pinchuk <sviatlana.pinchuk@fortnox.se>
2021-03-06 23:56:01 +0100

    addressbook modul creation
    initial commit
    test group creation
    test group deletion

---
+++ addressbook/src/test/java/addressbook/GroupCreationTests.java
@@ -0,0 +1,19 @@
+package addressbook;
+
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GroupCreationTests extends TestBase {
+
+  @Test
+  public void testGroupCreation() throws Exception {
+    gotoGroupPage();
+    initGroupCreation();
+    fillGroupForm(new GroupData("group1", "header group1", "footer group1"));
+    submitGroupCreation();
+    returnToGroupPage();
+  }
+
+}
